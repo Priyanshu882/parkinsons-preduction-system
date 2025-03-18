b2ec9b8eaf0e7d6d2437e3a81bf1437260e74eb2
Priyanshu Mudgal <101133173+Priyanshu882@users.noreply.github.com>
2025-03-18 22:48:25 +0530

    Update ParkinsonsPred.py

--- ParkinsonsPred.py
+++ ParkinsonsPred.py
@@ -1,7 +1,7 @@
 import os
 import pickle
 import streamlit as st
-from streamlit_option_menu import option_menu
+# from streamlit_option_menu import option_menu
 import base64
 
 # Set page configuration
